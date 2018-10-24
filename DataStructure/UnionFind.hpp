141c8e38f2ee3a46769a00e84b1192c12ee8523f
Tomoyasu Horino <horinoayame@gmail.com>
2018-10-25 01:01:27 +0900

    verify 済み

---
+++ DataStructure/UnionFind.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+
+template <typename T> struct UnionFind {
+  std::vector<T> data;
+  UnionFind(T n) : data(n, -1) {}
+  bool unite(T x, T y) {
+    x = root(x);
+    y = root(y);
+    if (x != y) {
+      if (data[y] < data[x])
+        swap(x, y);
+      data[x] += data[y];
+      data[y] = x;
+    }
+    return x != y;
+  }
+  T root(T x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
+  bool same(T x, T y) { return root(x) == root(y); }
+  T size(T x) { return -data[root(x)]; }
+};
