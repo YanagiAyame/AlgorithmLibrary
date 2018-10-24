8fa50d30d7edd31c7a5968161725fb8e0c1588de
Tomoyasu Horino <horinoayame@gmail.com>
2018-10-24 20:09:30 +0900

    BellmanFord法の追加とファイル名の変更
    verifyしていない

--- GraphAlgorithm/ShortestPath/BellmanFord.hpp
+++ GraphAlgorithm/ShortestPath/BellmanFord.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <cassert>
 #include <queue>
 #include <vector>
 
@@ -13,41 +14,40 @@ class Graph {
     long long weight;
     Edge(int src, int dst, long long weight)
         : src(src), dst(dst), weight(weight) {}
-
-    bool operator<(const Edge &e) const {
-      if (weight != e.weight)
-        return weight > e.weight;
-    }
   };
 
   std::vector<std::vector<Edge>> g;
   std::vector<long long> dist, prev;
+
+  bool negative_cycle = false;
 
 public:
   Graph(int n) : g(n), dist(n, INF64), prev(n, -1) {}
 
   void addEdge(int s, int t, int cost) { g[s].emplace_back(s, t, cost); }
 
-  std::vector<long long> dijkstra(int s) {
-    std::priority_queue<Edge> q;
+  std::vector<long long> BellmanFord(int s) {
+    const int n = g.size();
     dist[s] = 0;
-    for (q.emplace(-2, s, 0); !q.empty();) {
-      Edge e = q.top();
-      q.pop();
-      if (prev[e.dst] != -1) {
-        continue;
-      }
-      prev[e.dst] = e.src;
-      for (auto next_e : g[e.dst]) {
-        if (dist[next_e.dst] > next_e.weight + e.weight) {
-          dist[next_e.dst] = next_e.weight + e.weight;
-          q.emplace(next_e.src, next_e.dst, dist[next_e.dst]);
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        for (auto e : g[j]) {
+          if (dist[e.src] != INF64 && dist[e.dst] > dist[e.src] + e.weight) {
+            dist[e.dst] = dist[e.src] + e.weight;
+            prev[e.dst] = e.src;
+            if (i == n - 1) {
+              dist[e.dst] = -INF64;
+              negative_cycle = true;
+            }
+          }
         }
       }
     }
+    return dist;
   }
 
   std::vector<int> getPath(int t) {
+    assert(t != -INF64);
     std::vector<int> path;
     for (; t != -2; t = prev[t]) {
       path.push_back(t);
@@ -55,4 +55,6 @@ public:
     std::reverse(path.begin(), path.end());
     return path;
   }
+
+  bool NegativeCycle = negative_cycle;
 }
