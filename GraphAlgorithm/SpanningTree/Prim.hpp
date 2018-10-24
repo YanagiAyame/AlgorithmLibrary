57be17bd06bd0912f7b897ae086fb6e4e41182f6
Tomoyasu Horino <horinoayame@gmail.com>
2018-10-25 01:15:56 +0900

    verify 済み

---
+++ GraphAlgorithm/SpanningTree/Prim.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "template.hpp"
+#include <queue>
+#include <utility>
+
+std::pair<long long, Edges> prim(const Graph &g, int s) {
+  int n = g.size();
+  Edges tree;
+  long long total = 0;
+  std::vector<int> visited(n, 0);
+  std::priority_queue<Edge> q;
+  q.emplace(-1, s, 0);
+  while (!q.empty()) {
+    Edge e = q.top();
+    q.pop();
+    if (visited[e.dst])
+      continue;
+    tree.push_back(e);
+    total += e.weight;
+    visited[e.dst] = true;
+    for (auto f : g[e.dst]) {
+      if (!visited[f.dst])
+        q.push(f);
+    }
+  }
+  return std::pair<long long, Edges>(total, tree);
+}
