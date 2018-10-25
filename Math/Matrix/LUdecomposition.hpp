c1643b28493af34fe82614d57a3e42e74e45cc02
Tomoyasu Horino <horinoayame@gmail.com>
2018-10-25 16:14:52 +0900

    とりあえず LU 分解を追加した。 verify する。

---
+++ Math/Matrix/LUdecomposition.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "typedef.hpp"
+#include <cassert>
+#include <cmath>
+
+template <typename Type> struct lu_data {
+  Matrix<Type> A;
+  std::vector<int> pivots;
+};
+
+// A => LU
+// to do verifyする
+template <typename Type> lu_data LUdecomposer(Matrix<Type> A) {
+
+  assert(A.get_col() == A.get_row());
+
+  const int n = A.get_col();
+  std::vector<int> pivots(n);
+
+  for (int j = 0; j < n; ++j) {
+    // pivot選択
+    int pivot = j;
+    Type ma = std::fabs(A[pivot][j]);
+    for (int i = 0; i < n; ++i) {
+      abs_now = std::fabs(A[i][j]);
+      if (ma < abs_now) {
+        pivot = i;
+        ma = abs_now;
+      }
+    }
+    pivots[j] = i;
+
+    // pivot選択に従って入れ替え
+    std::swap(A[pivot], A[j]);
+
+    //三角行列を生成
+    for (int i = 0; i <= j; ++i) {
+      for (int k = 0; k < i; ++k) {
+        A[i][j] -= A[i][k] * A[k][j];
+      }
+    }
+    for (int i = j + 1; i < n; ++i) {
+      for (int k = 0; k < j; ++k) {
+        A[i][j] -= A[i][k] * A[k][j];
+      }
+      A[i][j] /= A[j][j];
+    }
+  }
+  return {A, pivots};
+}
+
+// find x : Ax = (LU)x = L(Ux) = Ly = b
+// to do verify
+template <typename Type>
+std::vector<Type> LUsolver(const lu_data &LU, std::vector<Type> b) {
+
+  const auto &A = LU.A;
+  const auto &pivots = LU.pivots;
+
+  const int n = pivots.size();
+
+  for (int i = 0; i < n; ++i) {
+    std::swap(b[i], b[pivots[n - i - 1]]);
+  }
+
+  // Ly = b を解く
+  //前進代入
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < i; ++j) {
+      b[i] -= A[i][j] * b[j];
+    }
+  }
+
+  // Ux = y を解く
+  //後退代入
+  for (int i = n - 1; i >= 0; --i) {
+    for (int j = i + 1; j < n; ++j) {
+      b[i] -= A[i][j] * b[j];
+    }
+    b[i] /= A[i][i];
+  }
+
+  return b;
+}
