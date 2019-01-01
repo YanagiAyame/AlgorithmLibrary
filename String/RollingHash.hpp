3373b676251aeb19ade728d73ad97f234fb134ed
Tomoyasu Horino <horinoayame@gmail.com>
2019-01-01 23:50:52 +0900

    基数をランダムにとるよう変更

--- String/RollingHash.hpp
+++ String/RollingHash.hpp
@@ -1,34 +1,63 @@
 #pragma once
 
-#include <map>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
-class RollingHash {
+using namespace std;
+
+template <unsigned kMod> class RollingHash {
   using ull = unsigned long long;
 
 private:
-  int n_;
-  std::vector<ull> hash1_, hash2_, pow1_, pow2_;
-  static const ull kBase1 = 1009;
-  static const ull kBase2 = 10007;
-  static const ull kMod1 = 999999937;
-  static const ull kMod2 = 1000000007;
+  const int _kN;
+  std::vector<unsigned> _hash, _pow;
+  unsigned _kBase;
 
 public:
   RollingHash(const std::string &s)
-      : n_(s.size()), hash1_(n_ + 1), hash2_(n_ + 1), pow1_(n_ + 1, 1),
-        pow2_(n_ + 1, 1) {
-    for (int i = 1; i <= n_; ++i) {
-      pow1_[i] = pow1_[i - 1] * kBase1 % kMod1;
-      pow2_[i] = pow2_[i - 1] * kBase2 % kMod2;
-      hash1_[i] = (hash1_[i - 1] * kBase1 + s[i - 1]) % kMod1;
-      hash2_[i] = (hash2_[i - 1] * kBase2 + s[i - 1]) % kMod2;
+      : _kN(s.size()), _hash(_kN + 1), _pow(_kN + 1, 1) {
+    std::random_device seed_gen;
+    std::mt19937_64 engine(seed_gen());
+    _kBase = engine() % (kMod - 2) + 2;
+    for (int i = 1; i <= _kN; ++i) {
+      _pow[i] = (unsigned)((ull)_pow[i - 1] * _kBase % kMod);
+      _hash[i] = (unsigned)(((ull)_hash[i - 1] * _kBase + s[i - 1]) % kMod);
     }
   }
-  // 0indexedで半開区間[l,r)
-  std::pair<ull, ull> get(int l, int r) {
-    return {(hash1_[r] - hash1_[l] * pow1_[r - l] % kMod1 + kMod1) % kMod1,
-            (hash2_[r] - hash2_[l] * pow2_[r - l] % kMod2 + kMod2) % kMod2};
+
+  // 0-indexedで半開区間[l,r)
+  ull get(int l, int r) {
+    return (_hash[r] - (ull)_hash[l] * _pow[r - l] % kMod + kMod) % kMod;
   }
 };
+
+/*
+int main() {
+  int n, m;
+  cin >> n >> m;
+  string s;
+  cin >> s;
+  RollingHash<1000000009> hashed_str1(s);
+  RollingHash<1000000007> hashed_str2(s);
+  int r = 1, l = 1;
+  set<pair<unsigned, unsigned>> sub;
+  for (int i = 0; i < m; ++i) {
+    string q;
+    cin >> q;
+    if (q == "L++") {
+      l++;
+    }
+    if (q == "L--") {
+      l--;
+    }
+    if (q == "R++") {
+      r++;
+    }
+    if (q == "R--") {
+      r--;
+    }
+    sub.insert({hashed_str1.get(l - 1, r), hashed_str2.get(l - 1, r)});
+  }
+  cout << sub.size() << endl;
+}
+*/
+// be checked by AOJ-2444.
