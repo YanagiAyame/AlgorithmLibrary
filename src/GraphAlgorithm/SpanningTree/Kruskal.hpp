a0240d9ed8c8fd85b2b1f248662c9e383278b509
Tomoyasu Horino <horinoayame@gmail.com>
2019-02-28 16:29:53 +0900

    add : Kruskal

--- src/GraphAlgorithm/SpanningTree/Kruskal.hpp
+++ src/GraphAlgorithm/SpanningTree/Kruskal.hpp
@@ -1,29 +1,45 @@
 #pragma once
 
-#include "../../DataStructure/UnionFind.hpp"
-#include "template.hpp"
 #include <queue>
 #include <utility>
 
-std::pair<long long, Edges> Kruskal(const Graph &g) {
-  int n = g.size();
-  UnionFind<int> uf(n);
-  std::priority_queue<Edge> q;
-  for (int i = 0; i < n; ++i) {
-    for (auto e : g[i]) {
-      if (i < e.dst)
+#include "../../DataStructure/union_find.hpp"
+#include "../graph_type.hpp"
+
+using Weight = long long;
+// Kruskal's algorithm
+// Finds a minimum-spanning-tree.
+// Returns the sum of the tree's cost and the edges of the tree.
+// Complexity : O(E log V)
+template <class... Args>
+auto Kruskal(const auto &g)
+{
+  const int n = g.size();
+  UnionFind uf(n);
+  std::priority_queue<Edge<Args...>> q;
+  for (int i = 0; i < n; ++i)
+  {
+    for (const auto &e : g[i])
+    {
+      if (i < e.to)
+      {
         q.push(e);
+      }
     }
   }
-  long long total = 0;
-  Edges forest;
-  while (forest.size() < n - 1 && !q.empty()) {
-    Edge e = q.top();
+  Weight total_cost = 0;
+  Graph<Args...> forest(n);
+  int forest_size = 0;
+  while (forest_size < n - 1 && !q.empty())
+  {
+    auto e = q.top();
     q.pop();
-    if (uf.unite(e.src, e.dst)) {
-      forest.push_back(e);
-      total += e.weight;
+    if (uf.unite(e.from, e.to))
+    {
+      forest.AddEdge(e);
+      forest_size++;
+      total_cost += std::get<0>(e.values);
     }
   }
-  return {total, forest};
+  return std::make_pair(total_cost, forest);
 }
