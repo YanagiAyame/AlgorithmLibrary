5cf8c15467fd3e9b0785bbb01bf8ed5a8b643a67
Tomoyasu Horino <horinoayame@gmail.com>
2019-08-27 22:02:37 +0900

    test : CI

--- test/DataStructure/binary_indexed_tree.test.cpp
+++ test/DataStructure/binary_indexed_tree.test.cpp
@@ -1,40 +1,7 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B"
 #include <src/DataStructure/binary_indexed_tree.hpp>
 
-#include <vector>
 #include <iostream>
-
-// Runs add queries and range sum queries.
-// Add : O(log n)
-// Sum : O(log n)
-template <class Abel>
-class BinaryIndexedTree
-{
-	const int size_;
-	std::vector<Abel> data_;
-
-public:
-	BinaryIndexedTree(int size) : size_(size), data_(size + 1, 0) {}
-
-	// data[target] += num
-	void Add(int target, Abel num)
-	{
-		for (int index = target + 1; index <= size_; index += index & -index)
-			data_[index] += num;
-	}
-
-	// Gets the sum of data[0, right)
-	Abel Sum(int right)
-	{
-		Abel sum = 0;
-		for (int index = right; index > 0; index -= index & -index)
-			sum += data_[index];
-		return sum;
-	}
-
-	// Gets the sum of data[left, right)
-	Abel Sum(int left, int right) { return Sum(right) - Sum(left); }
-};
 
 int main()
 {
